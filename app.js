3385cd84a63e606a6400a00e937411b85e1dccb9
Michael Ray <raymichael23@gmail.com>
2016-05-15 14:15:12 -0700

    changed from javascript constructor to written html for game board for testing purposes

--- app.js
+++ app.js
@@ -20,27 +20,42 @@ game = {
       } else if (game.currentPlayer == game.player2){
         game.currentPlayer = game.player1;
       }
+    },
+    // function to determine whether a player can make a given move or not
+    move: function(){
+            if (!$(this).hasClass('red') &&
+                !$(this).hasClass('black') && true) {
+                // check that this doesnt have a class of black or red.
+                // check that this either doesnt have a square below it
+                // or that the square below it has a class of red or black
+                // () {
+              console.log("boom");
+              $(this).addClass(game.currentPlayer.class);
+              game.functions.switchPlayer()
+            }
     }
   },
   init: function(){
           this.currentPlayer = game.player1;
           this.mark = game.currentPlayer.class;
           // builds the game board
-          for (var i=0; i<game.board.rows; i++) {
-            for (var u=0; u<game.board.columns; u++) {
-              document.querySelector('#container').innerHTML += '<div class="box"></div>'
-              if (u<game.board.columns-1) {
-                document.querySelector('#container').innerHTML += '<div class="divider"></div>'
-              }
-            }
-          };
-          $('.box').on('click', function(){
-            if (!$(this).hasClass('red') && !$(this).hasClass('black')) {
-              console.log("boom");
-              $(this).addClass(game.currentPlayer.class);
-              game.functions.switchPlayer()
-            }
-          });
+          // for (var i=0; i<game.board.rows; i++) {
+          //   // document.querySelector('#container').innerHTML += '<div class="row"'
+          //   for (var u=0; u<game.board.columns; u++) {
+          //     if (!u) {
+          //       document.querySelector('#container').innerHTML += '<div class="row">';
+          //     }
+          //     document.querySelector('#container').innerHTML += '<div class="box"></div>';
+          //     if (u<game.board.columns-1) {
+          //       document.querySelector('#container').innerHTML += '<div class="divider"></div>'
+          //     };
+          //     if (u == game.board.columns-1) {
+          //       document.querySelector('#container').innerHTML += '</div>'
+          //     };
+          //   };
+          //   // document.querySelector('#container').innerHTML += '</div>';
+          // };
+          $('.box').on('click', game.functions.move);
   },
 
 }

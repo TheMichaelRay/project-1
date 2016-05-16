e68050708bff5187a7e01b7deae3c87c55328c4f
Michael Ray <raymichael23@gmail.com>
2016-05-16 16:55:31 -0700

    refactored winchecks to be contained within move function and changed classchecks to check for current player before switchturns function instead of after

--- app.js
+++ app.js
@@ -42,40 +42,44 @@ game = {
                   $($down).hasClass(game.player2.class))
                 ) {
                     $circle.addClass(game.currentPlayer.class);
+                    game.functions.winDown(this);
+                    game.functions.winAcross(this);
+                    game.functions.winDiagLeft(this);
+                    game.functions.winDiagRight(this);
                     game.functions.switchPlayer()
                   }
     },
-    winDown: function() {
+    winDown: function(x) {
               var counter = 0;
-              var $circle = $(this);
+              var $circle = $(x);
               var $down = game.functions.$down($circle);
               for (var i = 0; i < 3; i ++)
-                if($($down).hasClass(game.opponent.class)) {
+                if($($down).hasClass(game.currentPlayer.class)) {
                   counter ++;
                   $circle = $($down);
                   $down = game.functions.$down($circle);
                 };
               if (counter >= game.connect-1) {
-                alert(game.opponent.name + " has won!")
+                alert(game.currentPlayer.name + " has won!")
               }
     },
-    winAcross: function() {
+    winAcross: function(x) {
                  var counter = 0;
-                 var $circle = $(this);
+                 var $circle = $(x);
                  var $left = game.functions.$left($circle);
                  var $right = game.functions.$right($circle);
                  for (var i = 0; i < 3; i ++) {
-                   if ($left.hasClass(game.opponent.class)) {
+                   if ($left.hasClass(game.currentPlayer.class)) {
                      counter ++;
                      $circle = $left;
                      $left = game.functions.$left($circle);
                    } else {
-                     $circle = $(this);
+                     $circle = $(x);
                      break;
                    }
                  };
                  for (var i = 0; i < 3; i++) {
-                   if ($right.hasClass(game.opponent.class)) {
+                   if ($right.hasClass(game.currentPlayer.class)) {
                      counter ++;
                      $circle = $right;
                      $right = game.functions.$right($circle);
@@ -84,26 +88,26 @@ game = {
                    }
                  };
                  if (counter >= game.connect-1) {
-                   alert (game.opponent.name + " has won!")
+                   alert (game.currentPlayer.name + " has won!")
                  }
                },
-    winDiagRight: function() {
+    winDiagRight: function(x) {
                     var counter = 0;
-                    var $circle = $(this);
+                    var $circle = $(x);
                     var $downRight = game.functions.$right(game.functions.$down($circle))
                     var $upLeft = game.functions.$left(game.functions.$up($circle));
                     for (var i = 0; i < 3; i ++) {
-                      if ($downRight.hasClass(game.opponent.class)) {
+                      if ($downRight.hasClass(game.currentPlayer.class)) {
                         counter ++;
                         $circle = $downRight;
                         $downRight = game.functions.$right(game.functions.$down($circle));
                       } else {
-                        $circle = $(this);
+                        $circle = $(x);
                         break;
                       }
                     };
                     for (var i = 0; i < 3; i ++) {
-                      if ($upLeft.hasClass(game.opponent.class)) {
+                      if ($upLeft.hasClass(game.currentPlayer.class)) {
                         counter ++;
                         $circle = $upLeft;
                         $upLeft = game.functions.$left(game.functions.$up($circle));
@@ -112,26 +116,26 @@ game = {
                     }
                     };
                     if (counter >= game.connect-1) {
-                      alert(game.opponent.name + " has won!")
+                      alert(game.currentPlayer.name + " has won!")
                     }
                   },
-    winDiagLeft: function() {
+    winDiagLeft: function(x) {
                     var counter = 0;
-                    var $circle = $(this);
+                    var $circle = $(x);
                     var $downLeft = game.functions.$left(game.functions.$down($circle));
                     var $upRight = game.functions.$right(game.functions.$up($circle));
                     for (var i = 0; i < 3; i ++) {
-                      if ($downLeft.hasClass(game.opponent.class)) {
+                      if ($downLeft.hasClass(game.currentPlayer.class)) {
                         counter ++;
                         $circle = $downLeft;
                         $downLeft = game.functions.$left(game.functions.$down($circle));
                       } else {
-                        $circle = $(this);
+                        $circle = $(x);
                         break;
                       }
                       };
                     for (var i = 0; i < 3; i ++) {
-                      if ($upRight.hasClass(game.opponent.class)) {
+                      if ($upRight.hasClass(game.currentPlayer.class)) {
                         counter ++;
                         $circle = $upRight;
                         $upRight = game.functions.$right(game.functions.$up($circle));
@@ -140,7 +144,7 @@ game = {
                       }
                       };
                     if (counter >= game.connect-1) {
-                      alert(game.opponent.name + " has won!")
+                      alert(game.currentPlayer.name + " has won!")
                     }
 
                     }
@@ -156,10 +160,10 @@ game = {
             $('#container').children('.box').wrapAll('<div class="row"></div>')
           };
           $('.box').click(game.functions.move);
-          $('.box').click(game.functions.winDown);
-          $('.box').click(game.functions.winAcross);
-          $('.box').click(game.functions.winDiagLeft);
-          $('.box').click(game.functions.winDiagRight);
+          // $('.box').click(game.functions.winDown);
+          // $('.box').click(game.functions.winAcross);
+          // $('.box').click(game.functions.winDiagLeft);
+          // $('.box').click(game.functions.winDiagRight);
   },
 
 }

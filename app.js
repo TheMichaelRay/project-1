08d05fb52389e54b5d5193f5ddb015a8bec37a41
Michael Ray <raymichael23@gmail.com>
2016-05-15 22:08:13 -0700

    added function to check down and to the right, not currently working

--- app.js
+++ app.js
@@ -53,9 +53,59 @@ game = {
     winAcross: function() {
                  var counter = 0;
                  var $circle = $(this);
-                 console.log($circle.next().hasClass(game.currentPlayer.class));
-                 console.log($circle.prev().hasClass(game.currentPlayer.class))
-    }
+                 var $left = $circle.prev();
+                 var $right = $circle.next();
+                 for (var i = 0; i < 3; i ++) {
+                   if ($left.hasClass(game.currentPlayer.class)) {
+                     counter ++;
+                     $circle = $left;
+                     $left = $circle.prev();
+                   } else {
+                     $circle = $(this);
+                     break;
+                   }
+                 };
+                 for (var i = 0; i < 3; i++) {
+                   if ($right.hasClass(game.currentPlayer.class)) {
+                     counter ++;
+                     $circle = $right;
+                     $right = $circle.next();
+                   } else {
+                     break;
+                   }
+                 };
+                 if (counter === 3) {
+                   alert ('winner')
+                 }
+               },
+    winDiagRight: function() {
+                   var counter = 0;
+                   var $circle = $(this);
+                   var $downRight = $circle.parent().next().children()[$circle.index()].next();
+                   var $upLeft = $circle.parent().prev().children()[$circle.index()].prev();
+                   for (var i = 0; i < 3; i ++) {
+                     if ($downRight.hasClass(game.currentPlayer.class)) {
+                       counter ++;
+                       $circle = $downRight;
+                       $downRight = $circle.parent().next().children()[$circle.index()].next();
+                     } else {
+                       $circle = $(this);
+                       break;
+                     }
+                   };
+                   for (var i = 0; i < 3; i ++) {
+                     if ($upLeft.hasClass(game.currentPlayer.class)) {
+                       counter ++;
+                       $circle = $upLeft;
+                       $upLeft = $circle.parent().prev().children()[$circle.index()].prev();
+                     } else {
+                       break;
+                     }
+                   };
+                   if (counter === 3) {
+                     alert('winner')
+                   }
+                  }
   },
   init: function(){
           this.currentPlayer = game.player1;
@@ -79,7 +129,8 @@ game = {
           // };
           $('.box').on('click', game.functions.move);
           $('.box').on('click', game.functions.winDown);
-          $('.box').on('click', game.functions.winAcross)
+          $('.box').on('click', game.functions.winAcross);
+          $('.box').on('click', game.functions.winDiagRight);
           $('.box').on('click', game.functions.switchPlayer);
   },
 
